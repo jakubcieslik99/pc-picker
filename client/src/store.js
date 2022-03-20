e88952ed9803626dc33f15d0024ee50623c8aa2a
Jakub Cieślik <jakub.cieslik.99@gmail.com>
2022-03-20 12:49:15 +0100

    Applying Redux Devtools

--- client/src/store.js
+++ client/src/store.js
@@ -44,8 +44,8 @@ const reducer = combineReducers({
     deleteManagedConfig: deleteManagedConfigReducer
 })
 
-//const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-//const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)))
-const store = createStore(reducer, initialState, compose(applyMiddleware(thunk)))
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)))
+//const store = createStore(reducer, initialState, compose(applyMiddleware(thunk)))
 
 export default store
